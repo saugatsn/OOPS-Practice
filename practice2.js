728c3495cc581712f9ec8e15a8aa82dcb8d58173
saugatsn <nepal.saugat.sn@gmail.com>
2024-01-09 00:03:20 +0545

    classes

--- practice2.js
+++ practice2.js
@@ -1,10 +1,12 @@
-function personMaker(name, age) {
-  this.name = name;
-  this.age = age;
-  console.log(this);
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  talk() {
+    console.log(`Hi, this is ${this.name}`);
+  }
 }
-personMaker.prototype.talk = function () {
-  console.log(`Hello, I am ${this.name}`);
-};
-let p1 = new personMaker("adam", 23);
-let p2 = new personMaker("eve", 23);
+
+let p1 = new Person("Sagar", 26);
+let p2 = new Person("Samip", 32);
